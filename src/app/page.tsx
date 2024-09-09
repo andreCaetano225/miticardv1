e243243691d31cde63b2d73297f23fb204a40a5c
André de Sousa <andre.caetano2007@hotmail.com>
2024-09-09 20:14:20 -0300

    resolver build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { About } from "@/components/About";
 import { Services } from "@/components/Services";
 import { StoreServices } from "@/components/StoreServices";
-import Image from "next/image";
 
 export default function Home() {
   return (
